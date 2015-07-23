b20c348f751b338537d63a4a97f12e26e4692a38
Kgwkgwkgw <kgwaaa@naver.com>
2015-07-23 20:32:43 +0900

    0723

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
       user.uid      = auth.uid
       user.name     = auth.info.name
       user.token    = auth.token
-      
       user.save
     end
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,4 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '851870488230120', "495190d07ea7c87736e5592126bcd98f"
-end
+end 
