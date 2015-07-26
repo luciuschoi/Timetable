c2c4fa771d0357f956b18fea0130c8648f759e0b
Ho Ruby <qqwq9999@gmail.com>
2015-07-26 14:15:59 +0900

    add comment#destroy , modify comment#create via jQuery

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,16 @@ class CommentsController < ApplicationController
 			@comment = current_user.comments.build(lecture_id: params[:lecture_id],
 									content: params[:comment][:content])
 
+			@comments = Comment.where("lecture_id = ?", params[:lecture_id]).order('created_at DESC')
+
 			@comment.save
 
-			redirect_to lecture_path(params[:lecture_id])
+			respond_to do |format|
+				format.js
+				format.html {redirect_to lecture_path(params[:lecture_id])}
+			end
+
+			
 		else
 			redirect_to "/auth/facebook", id: "sign_in" 
 		end
@@ -30,16 +37,28 @@ class CommentsController < ApplicationController
 		@lecture = Lecture.find_by(id: params[:lecture_id])
 		@comments = Comment.where("lecture_id = ?", @lecture.id).order('created_at DESC')
 		if @comment.update_attributes(comment_params)
-
 			respond_to do |format|
-				format.html {redirect_to @lecture}
 				format.js
+				format.html {redirect_to @lecture}
 			end	
 		else
 
 			render @lecture
 		end
 		
+	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		@lecture = Lecture.find_by(id: @comment.lecture_id)
+		@comments = Comment.where("lecture_id = ?", @lecture.id).order('created_at DESC')
+
+		@comment.destroy
+
+		respond_to do |format|
+			format.js
+			format.html {redirect_to @lecture}
+		end
 	end
 
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
 
-	validates :content, :length => { :minimum => 10, :maximum => 500 }
+	validates :content, :length => { :minimum => 1, :maximum => 500 }
 
 	belongs_to :user
 	belongs_to :lecture	
 end
+
