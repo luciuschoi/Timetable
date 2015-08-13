45fc0c229ffd3f66507dae8cd42e1a846d6b4c19
Ho Ruby <qqwq9999@gmail.com>
2015-08-13 09:51:35 +0900

    0813 환경설정 수

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,14 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.compress = false
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile =  ['*.js', '*.scss', '*.css.erb'] 
+  #config.assets.precompile =  ['*.js', '*.scss', '*.css.erb'] 
+  config.assets.precompile += %w( 'application.js', 'application.css')
+                                 #specify CSS or JS files inside here! )
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
