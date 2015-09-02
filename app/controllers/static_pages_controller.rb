22b8709a16783d392c951bdbec940b9751ef15da
Kgwkgwkgw <kgwkgwkgw@naver.com>
2015-09-02 19:02:20 +0900

    0902 머지전

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
    before_action :fillnickname, only: [:home]
-
+   before_action :login_check, only:[:forcingwritting]
   def home
 
     # 검색 했니?
@@ -81,6 +81,11 @@ class StaticPagesController < ApplicationController
     end
   end
 
-  
+  def login_check
+    unless user_login?
+        redirect_to root_url
+    end
+
+  end
   
 end
