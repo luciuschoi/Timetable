d069e840106a2c2f6dc78bb90498a72e36713cd2
Ho Ruby <qqwq9999@gmail.com>
2015-09-24 23:37:39 +0900

    0924 에러 메시지 수정 및 회원가입 뷰 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,6 @@ class User < ActiveRecord::Base
 
    validates :email, presence: true, length: {maximum: 155}, 
                     uniqueness: true
-
-
-   validates :password, presence: true, length: { minimum: 6 }
-   has_secure_password
-
-
-
    validates :password, presence: true, length: { minimum: 6 }
    has_secure_password
 
