02e27ccbd9ff2790b5ea7173933407b1a71b1a31
Ho Ruby <qqwq9999@gmail.com>
2015-12-12 23:42:30 +0900

    no message

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -29,7 +29,7 @@ class Lecture < ActiveRecord::Base
   #   (2..spreadsheet.last_row).each do |i|
   #     row = Hash[[header, spreadsheet.row(i)].transpose]
   #     lecture = find_by_id(row["id"]) || new
-  #     lecture.attributes = row.to_hash.slice("subject", "professor", "major","lecturetime", "place")
+  #     lecture.attributes = row.to_hash.slice("subject", "professor", "major","lecturetime", "place", "isu")
 
   #     lecture.save
   #   end
