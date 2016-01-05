7a02bc2a1f77ec3c2abee1bcac6a9d92383ab855
Ho Ruby <qqwq9999@gmail.com>
2016-01-05 14:32:59 +0900

    0105 시간표에서 과목 삭제할 때 동일한 lecture_id를 가진 강의도 삭제되는(즉, 2개 강의가 삭제되는) 에러 해결

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -45,8 +45,10 @@ class Lecture < ActiveRecord::Base
       # lecture = find_by_id(row["id"]) || new
       # lecture.update_attribute("isu", row["isu"] )
       # lecture.update_attribute("place", row["place"] )
-      @lecture.update_attribute("semester", row["semester"])
-      @lecture.save
+      if @lecture
+        @lecture.update_attribute("semester", row["semester"])
+        @lecture.save
+      end
     end
   end
   
@@ -175,7 +177,7 @@ class Lecture < ActiveRecord::Base
 
   def self.search_timetable(search, semester)
     unless search.nil?
-      where(['professor LIKE ? OR subject LIKE ? OR major LIKE ? AND semester LIKE ?',
+      where(['(professor LIKE ? OR subject LIKE ? OR major LIKE ?)AND semester LIKE ?',
              "#{search}%","#{search}%","#{search}%", "#{semester}"])
     end
   end  
