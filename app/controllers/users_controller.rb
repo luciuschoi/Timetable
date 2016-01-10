8489801771ae6220f87d0cc3e2e6a998ddee37ad
Ho Ruby <qqwq9999@gmail.com>
2016-01-10 23:34:40 +0900

    임시 커밋

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      redirect_to :controller => 'static_pages', :action => 'rank'
+      redirect_to :controller => 'static_pages', :action => 'new_timetable_path'
     else
       render 'new'
     end
