41e1f9f69dec4c6132ac860bf1eb064bc0b7fb9a
Ho Ruby <qqwq9999@gmail.com>
2016-01-16 19:19:25 +0900

    Converting SQLite3 to MYSQL

--- db/migrate/20150927085532_add_lectureid_to_timetables.rb
+++
@@ -1,5 +0,0 @@
-class AddLectureidToTimetables < ActiveRecord::Migration
-  def change
-    add_column :timetables, :lecture_id, :integer
-  end
-end
--- db/migrate/20151128154940_rename_timetables_to_enrollments.rb
+++
@@ -1,5 +0,0 @@
-class RenameTimetablesToEnrollments < ActiveRecord::Migration
-  def change
-  	rename_table :timetables, :enrollments
-  end
-end
