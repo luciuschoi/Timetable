cf259e7f5de52b3b6e2ce3a588580bbca675f925
Ho Ruby <qqwq9999@gmail.com>
2016-01-17 18:30:35 +0900

    convert sqlite3 to mysql2....plz

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
  # get 'users/new'
  get 'home_admin' => 'static_pages#home_admin'
  get 'lecture_search' => 'static_pages#search'
